e84236cab9dd100d1f983393658244891ed18fd4
Jacqueline Alves Coutinho <jacqueline.a.coutinho@gmail.com>
2023-08-30 22:40:55 -0400

    first commit

---
+++ resources/views/contact-post.blade.php
@@ -0,0 +1,4 @@
+<strong>Name :</strong> {{ $data->name }} <br>
+<strong>Phone :</strong> {{ $data->phone }} <br>
+<strong>Email :</strong> {{ $data->email }} <br>
+<strong>Message :</strong> {{ $data->message }} <br>
